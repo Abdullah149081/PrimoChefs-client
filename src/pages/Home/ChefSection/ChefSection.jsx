9480179f7eee3c3fe81953823625e26b4e858793
Abdullah149081 <masudmd425@gmail.com>
2023-05-03 00:47:01 +0600

    update btn text color

--- src/pages/Home/ChefSection/ChefSection.jsx
+++ src/pages/Home/ChefSection/ChefSection.jsx
@@ -1,27 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ChefSection = ({ chefs }) => {
   console.log(chefs);
   const { id, chef_picture, chef_name, years_of_experience, num_of_recipes, likes } = chefs;
   return (
     <div>
-      <div className="card w-full border bg-base-100 shadow-xl">
-        <figure className="px-4 pt-4">
-          <img src={chef_picture} alt="chef_picture" className="rounded-xl border" />
+      <div className="card w-full  bg-base-100 rounded-lg border">
+        <figure className="">
+          <img src={chef_picture} alt="chef_picture" className="rounded-xl " />
         </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">{chef_name}</h2>
-          <p className="text-details ">
-            Years of experience: <span className="text-g">{years_of_experience}</span>
+        <div className="card-body space-y-4 ">
+          <h2 className="card-title text-[#757575]">{chef_name}</h2>
+          <p className="text-details">
+            Years of experience: <span>{years_of_experience}</span>
           </p>
           <p className="text-details ">
-            Numbers of recipes: <span className="text-g">{num_of_recipes}</span>
+            Numbers of recipes: <span>{num_of_recipes}</span>
           </p>
           <p className="text-details text-g">likes: {likes}</p>
           <div className="card-actions">
-            <button type="button" className="btn btn-primary">
-              View Recipes
-            </button>
+            <Link to="/">
+              <button type="button" className="btn btn-primary">
+                View Recipes
+              </button>
+            </Link>
           </div>
         </div>
       </div>
