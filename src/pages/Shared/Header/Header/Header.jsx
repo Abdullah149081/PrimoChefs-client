53a3a40caf394178b0d260980feca6ef46bd4da3
Abdullah149081 <masudmd425@gmail.com>
2023-05-02 22:15:43 +0600

    Error page done

--- src/pages/Shared/Header/Header/Header.jsx
+++ src/pages/Shared/Header/Header/Header.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useState } from "react";
-import { FiCommand } from "react-icons/fi";
+import { FaAlignRight } from "react-icons/fa";
+
 import { TiTimesOutline } from "react-icons/ti";
 import { Link } from "react-router-dom";
 import ActiveLink from "../ActiveLink/ActiveLink";
@@ -14,8 +15,7 @@ const Header = () => {
         <div>
           <Link to="/">
             <h2 className="text-4xl font-bold">
-              Pri<span className="font-extrabold text-transparent  bg-clip-text bg-gradient-to-r from-lime-600 to-slate-700">m</span>i
-              <span className="font-extrabold text-transparent  bg-clip-text bg-gradient-to-r from-lime-600 to-slate-700">o</span>Chefs
+              <span className="font-extrabold text-transparent  bg-clip-text bg-gradient-to-r from-[#068F3D] via-black to-black">PrimoChefs</span>
             </h2>
           </Link>
         </div>
@@ -29,18 +29,18 @@ const Header = () => {
             tabIndex="0"
           >
             <span className="pr-12 md:pr-0 ">
-              {open ? <TiTimesOutline className="h-6 w-7 mt-7 md:hidden text-gray-900 cursor-pointer" /> : <FiCommand className="h-7 w-7 mt-7 md:hidden text-gray-900  cursor-pointer" />}
+              {open ? <TiTimesOutline className="h-6 w-7 mt-7 md:hidden text-gray-900 cursor-pointer" /> : <FaAlignRight className="h-5 w-5 mt-7 md:hidden text-gray-900  cursor-pointer" />}
             </span>
           </div>
 
           <div
             className={`flex  flex-col  md:py-0 md:space-x-10  font-bold  md:shadow-none   md:static md:flex-row  md:block ${
-              open ? "top-22 left-0 h-1/2 w-full text-center py-12   shadow-lg pl-12 pr-6 space-y-6   absolute z-10 bg-white" : "hidden"
+              open ? "top-22 left-0  w-full text-center py-12   shadow-lg pl-12 pr-6 space-y-6   absolute z-10 bg-white" : "hidden"
             }`}
           >
             <ActiveLink to="/">Home</ActiveLink>
             <ActiveLink to="/blog">Blog</ActiveLink>
-            <button type="button" className="btn block  lg:hidden ">
+            <button type="button" className="btn  mx-auto w-2/4 block  lg:hidden ">
               login
             </button>
           </div>
