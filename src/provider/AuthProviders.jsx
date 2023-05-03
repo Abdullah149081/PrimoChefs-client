c8c6bdc2824a2fa84539d065d37fbe5cc4e2919a
Abdullah149081 <masudmd425@gmail.com>
2023-05-03 12:36:44 +0600

    sign up user firebase done

--- src/provider/AuthProviders.jsx
+++ src/provider/AuthProviders.jsx
@@ -1,11 +1,19 @@
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 import React, { createContext, useMemo, useState } from "react";
+import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
 const AuthProviders = ({ children }) => {
   const [user, setUser] = useState(null);
 
-  const authInfo = useMemo(() => ({ user }), [user]);
+  // sign-up user
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const authInfo = useMemo(() => ({ user, createUser }), [user]);
 
   return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
